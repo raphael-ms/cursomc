c46aa10cb5fe1584d5d0d35e736ce1ae4149257f
Raphael Marques <raphaelmarques77@gmail.com>
2018-07-31 21:29:26 -0300

    Usando apenas JsonIgnore

--- src/main/java/com/raphael/cursomc/domain/Categoria.java
+++ src/main/java/com/raphael/cursomc/domain/Categoria.java
@@ -9,9 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 @Entity
 public class Categoria implements Serializable{
 
@@ -21,7 +18,7 @@ public class Categoria implements Serializable{
 	private Integer id;
 	private String nome;
 	
-	@JsonManagedReference
+	
 	@ManyToMany(mappedBy="categorias")
 	private List<Produto> produtos = new ArrayList<>();
 	
--- src/main/java/com/raphael/cursomc/domain/Produto.java
+++ src/main/java/com/raphael/cursomc/domain/Produto.java
@@ -14,7 +14,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.JoinColumn;
@@ -30,7 +29,7 @@ public class Produto implements Serializable{
 	private String nome;
 	private Double preco;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA", 
 		joinColumns = @JoinColumn(name = "produto_id"),
