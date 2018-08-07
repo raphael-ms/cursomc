33f2e7e03e3ad934b75c596836752507dd1f7e0d
Raphael Marques <raphaelmarques77@gmail.com>
2018-08-06 21:53:01 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/raphael/cursomc/services/CategoriaService.java
+++ src/main/java/com/raphael/cursomc/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.raphael.cursomc.domain.Categoria;
@@ -52,6 +55,11 @@ public class CategoriaService {
 		}catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 	
 }
